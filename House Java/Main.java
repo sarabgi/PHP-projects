28f31217574de6e9d85200a8bd5e44712a501d2e
rollingjizas <93259134+rollingjizas@users.noreply.github.com>
2022-10-29 11:32:55 +0800

    Add files via upload

---
+++ House Java/Main.java
@@ -0,0 +1,31 @@
+	public class Main {
+
+  
+  
+    public static void main(String[] args) {
+
+  
+
+      class_roof layerA = new class_roof();
+       
+ 	class_wall layerB = new class_wall();
+
+
+ 
+       	layerA.roof();
+
+        layerA.line();
+    
+    	layerB.wall();
+      
+  	layerA.line();
+ 
+       System.out.println("         |_|   _|___|_   |_|");
+ 
+       System.out.println("         |_|  _|_____|_  |_|");
+   
+
+}
+
+
+}
